c755a0b3b55a7cbf958d3d4221ea87db30eab9fc
Ronan Klyne <ronan.klyne@virtualstock.co.uk>
2016-08-09 11:41:11 +0100

    Initial commit. Log storage app.
    Include migrations in package.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='log_storage',
+    version='0.2',
+    description='Log handler to store messages with a database record and optional file storage.',
+    author='Virtualstock',
+    author_email='development.team@virtualstock.com',
+    url='https://github.com/virtualstock/log_storage/',
+    packages=['log_storage', 'log_storage.migrations'],
+    include_package_data=True,
+    install_requires=['django'],
+)
