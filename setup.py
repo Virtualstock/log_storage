7dc84f583aafe4c4be0d8e9fd4fd9e45007c3c4b
Ronan Klyne <ronan.klyne@virtualstock.co.uk>
2016-08-30 13:16:22 +0100

    Include migrations in package.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name='log_storage',
-    version='0.1',
+    version='0.2',
     description='Log handler to store messages with a database record and optional file storage.',
     author='Ronan Klyne',
     author_email='ronan.klyne@virtualstock.co.uk',
     url='http://pypi.v-source.co.uk/',
-    packages=['log_storage',],
+    packages=['log_storage', 'log_storage.migrations'],
     include_package_data=True,
     install_requires=[]
 )
