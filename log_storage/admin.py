c852bfd2567f5e69a02ba9e35b3f0ced3c2c4a21
Bernardo Silva <benny.stuff@gmail.com>
2018-09-04 18:54:26 +0100

    add more fields by default to django admin to make the Logs more useful

--- log_storage/admin.py
+++ log_storage/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Log
 
+
 class LogAdmin(admin.ModelAdmin):
-    readonly_fields = ('log_data', )
+    readonly_fields = ('log_data', 'db_log_data', 'filename', 'save_file',)
+    list_display = ('filename', 'created', 'save_file',)
+    list_filter = ('created', 'save_file', 'filename',)
+    search_fields = ('filename',)
+
 
 admin.site.register(Log, LogAdmin)
 
