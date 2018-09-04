d86f69fe0d81b7b086ea07b42f7c848cc51832a0
Bernardo Silva <benny.stuff@gmail.com>
2018-09-04 20:03:11 +0100

    SDD-1280 remove the filename filter since most of the times filename is always difference

--- log_storage/admin.py
+++ log_storage/admin.py
@@ -5,7 +5,7 @@ from .models import Log
 class LogAdmin(admin.ModelAdmin):
     readonly_fields = ('log_data', 'db_log_data', 'filename', 'save_file',)
     list_display = ('filename', 'created', 'save_file',)
-    list_filter = ('created', 'save_file', 'filename',)
+    list_filter = ('created', 'save_file',)
     search_fields = ('filename',)
 
 
