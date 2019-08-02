cf9439b1825d4069fe59e0603584f6d0a40eeeb3
jon <jonathan.ady@virtualstock.co.uk>
2019-08-02 13:03:56 +0100

    Simplify desired file name creation, blacken

--- log_storage/models.py
+++ log_storage/models.py
@@ -9,32 +9,32 @@ class BaseLog(models.Model):
     db_log_data = models.TextField()
     filename = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
-    prefix = ''
-    suffix = '.log'
+    prefix = ""
+    suffix = ".log"
 
     class Meta:
         abstract = True
 
     def _get_desired_filename(self):
-        import os, time, random
+        import os, time
         from django.conf import settings
-        path = getattr(settings, 'LOG_DATA_DIR', 'logs')
-        filename = '_'.join(filter(bool, [self.prefix,
-                    time.strftime('%Y%m%d_%H%M%S'),
-                    ''.join([random.choice('0123456789') for i in range(0, 5)]),
-                    self.suffix]))
+
+        path = getattr(settings, "LOG_DATA_DIR", "logs")
+        filename = "{}_{}{}".format(
+            self.prefix, time.strftime("%Y%m%d_%H%M%S_%f"), self.suffix
+        )
         return os.path.join(path, filename)
 
     @property
     def log_data(self):
         if self.save_file:
             if self.filename:
-                with private_storage.open(self.filename, 'rb') as f:
-                    return f.read().decode('utf-8')
+                with private_storage.open(self.filename, "rb") as f:
+                    return f.read().decode("utf-8")
             else:
-                return u''
+                return u""
         else:
-            return self.db_log_data or u''
+            return self.db_log_data or u""
 
     _handler = None
 
@@ -44,7 +44,7 @@ class BaseLog(models.Model):
         class FileStream(object):
             def __init__(stream):
                 stream.contents = ContentFile(None)
-                stream.contents.open('wb')
+                stream.contents.open("wb")
 
             def write(stream, data):
                 stream.contents.write(data)
@@ -52,25 +52,34 @@ class BaseLog(models.Model):
         class FileStreamHandler(StreamHandler):
             def close(handler):
                 handler.stream.contents.seek(0)
-                self.filename = private_storage.save(self._get_desired_filename(), handler.stream.contents)
+                self.filename = private_storage.save(
+                    self._get_desired_filename(), handler.stream.contents
+                )
                 super(FileStreamHandler, handler).close()
+
         return FileStreamHandler(FileStream())
 
     def make_db_handler(self):
         from logging import StreamHandler
-        self.db_log_data = self.db_log_data or u''
+
+        self.db_log_data = self.db_log_data or u""
+
         class DbStream(object):
             def write(stream, data):
                 self.db_log_data += data
+
             def flush(stream):
                 self.save()
+
             close = flush
+
         return StreamHandler(DbStream())
 
     @property
     def formatter(self):
         from logging import Formatter
-        return Formatter('%(asctime)s   %(levelname)s   %(name)s   %(message)s')
+
+        return Formatter("%(asctime)s   %(levelname)s   %(name)s   %(message)s")
 
     def make_handler(self):
         if self.save_file:
@@ -82,11 +91,13 @@ class BaseLog(models.Model):
 
     def __enter__(self):
         import logging
+
         self._handler = self.make_handler()
         logging.root.addHandler(self._handler)
 
     def __exit__(self, *tpl):
         import logging
+
         logging.root.removeHandler(self._handler)
         self._handler.close()
         self._handler = None
